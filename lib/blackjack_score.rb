9cd00716de78c40296b0ed1610d150e4264ea232
Kareha Agesa <kmaagesa@gmail.com>
2020-09-16 20:08:07 -0700

    adding tests and main method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,48 @@
 # blackjack_score.rb
 
+# is 10 a valid card?
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+
+
+def add_in_aces(sum)
+  if sum + 11 > 21
+    return sum += 1
+  else
+    return sum += 11
+  end
+end
 
 def blackjack_score(hand)
 
+  if hand.length > 5
+    raise ArgumentError.new("hand greater than 5")
+  end
+
+  # if it's a string, capitalize 1st value only
+  hand = hand.map{|value| value.is_a?(String) ? value.capitalize : value}
+
+  # handles test for invalid card
+  unless (hand - VALID_CARDS).empty?
+    raise ArgumentError.new("You have entered an invalid card value")
+  end
+
+  sum = 0
+  # get the total for all numeric cards
+  sum += hand.select{|value| value.is_a?(Integer)}.reduce(:+).to_i
+
+  # add the values for face cards
+  sum += hand.select{|value| ["Jack", "Queen", "King"].include?(value)}.length * 10.to_i
+
+  # deal with aces
+  hand.select{|value| value == "Ace"}.length.times do
+    sum = add_in_aces(sum)
+  end
+
+  if sum > 21
+    raise ArgumentError.new("Score exceeds 21")
+  end
+
+  return sum
 end
+
+puts blackjack_score(["Jack", "Ace"])
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -9,6 +9,11 @@ require_relative '../lib/blackjack_score'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Blackjack Score' do
+
+  it "hand cannot exceed 5 values" do
+    expect {blackjack_score([9, 2, 3, 4, 5, 6])}.must_raise ArgumentError
+  end
+
   it 'can calculate the score for a pair of number cards' do
 
     # Arrange
@@ -17,27 +22,32 @@ describe 'Blackjack Score' do
     # Act
     score = blackjack_score(hand)
 
-    # Assert <-  You do this part!
+    # Assert
+    expect(score).must_equal 7
 
   end
 
   it 'facecards have values calculated correctly' do
-
+    expect(blackjack_score(["King", "Queen"])).must_equal 20
+    expect(blackjack_score(["Queen", 4, 2])).must_equal 16
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
-
+    expect(blackjack_score([2, 2, "Ace"])).must_equal 15
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
-
+    expect(blackjack_score(["King", "Queen", "Ace"])).must_equal 21
+    expect(blackjack_score(["Jack", "Ace"])).must_equal 21
   end
 
   it 'raises an ArgumentError for invalid cards' do
-
+    expect {blackjack_score(["banana"])}.must_raise ArgumentError
+    expect {blackjack_score([0, 8])}.must_raise ArgumentError
+    expect {blackjack_score([-1, 3, "King"])}.must_raise ArgumentError
   end
 
   it 'raises an ArgumentError for scores over 21' do
-
+    expect {blackjack_score(["Queen", "King", 4])}.must_raise ArgumentError
   end
 end
